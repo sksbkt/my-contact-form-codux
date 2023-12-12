a0ab40d1336fdfe69f63160c41141b3fb937f032
atenziono <atenziono@gmail.com>
2023-12-12 14:42:39 +0330

    creating a new form

---
+++ src/components/submit-button/submit-button.tsx
@@ -0,0 +1,19 @@
+import classNames from 'classnames';
+import styles from './submit-button.module.scss';
+
+export interface SubmitButtonProps {
+    className?: string;
+    children?:JSX.Element|Array<JSX.Element|string>|string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const SubmitButton = ({ className,children }: SubmitButtonProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <button>{children}</button>
+        </div>
+    );
+};
